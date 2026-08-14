6bf29f7a2e006297a03eb123020107e4788ce8c4
Sarin-jacob <sarin.c.jacob2001@gmail.com>
2026-08-14 17:27:11 +0530

    added show hide pass

--- frontend/src/lib/Onboarding.svelte
+++ frontend/src/lib/Onboarding.svelte
@@ -38,7 +38,8 @@
                 name,
                 type,
                 value: (type === 'bool' || type === 'boolean' ? 'false' : ''),
-                confirmValue: ''
+                confirmValue: '',
+                showPassword: false
             }));
 
         } catch (err) {
@@ -188,15 +189,43 @@
                     <form on:submit={handleSetup} class="space-y-6">
                         
                         <!-- DYNAMIC FORM GENERATOR -->
-                        {#each formFields as field}
+                        {#each formFields as field,i}
                             <div class="form-control">
                                 <label class="label"><span class="label-text font-bold capitalize">{field.name.replace(/_/g, ' ')}</span></label>
                                 
                                 {#if field.type === 'secret'}
-                                    <input type="password" bind:value={field.value} required class="input input-bordered input-primary w-full" placeholder="Enter {field.name.replace(/_/g, ' ')}" />
+                                    <div class="relative w-full">
+                                        <input 
+                                            type={field.showPassword ? "text" : "password"} 
+                                            bind:value={field.value} 
+                                            required 
+                                            class="input input-bordered input-primary w-full pr-12" 
+                                            placeholder="Enter {field.name.replace(/_/g, ' ')}" 
+                                        />
+                                        
+                                        <button 
+                                            type="button" 
+                                            class="absolute top-1/2 right-3 -translate-y-1/2 opacity-60 hover:opacity-100 transition-opacity focus:outline-none"
+                                            on:click={() => formFields[i].showPassword = !formFields[i].showPassword}
+                                            title={field.showPassword ? "Hide password" : "Show password"}
+                                        >
+                                            {#if field.showPassword}
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" /></svg>
+                                            {:else}
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" /></svg>
+                                            {/if}
+                                        </button>
+                                    </div>
                                     <label class="label mt-2"><span class="label-text font-bold capitalize">Confirm {field.name.replace(/_/g, ' ')}</span></label>
-                                    <input type="password" bind:value={field.confirmValue} required class="input input-bordered input-primary w-full" placeholder="Confirm {field.name.replace(/_/g, ' ')}" />
-                                    
+                                    <div class="relative w-full">
+                                        <input 
+                                            type={field.showPassword ? "text" : "password"} 
+                                            bind:value={field.confirmValue} 
+                                            required 
+                                            class="input input-bordered input-primary w-full pr-12" 
+                                            placeholder="Confirm {field.name.replace(/_/g, ' ')}" 
+                                        />
+                                    </div>
                                 {:else if field.type === 'textarea'}
                                     <textarea bind:value={field.value} required rows="4" class="textarea textarea-bordered textarea-primary font-mono text-sm leading-relaxed"></textarea>
                                     
